02359e542c807fc53874b212ac4b7d1a0385127b
pk1997 <skpavan2@gmail.com>
2018-11-05 18:42:52 +0530

    added hoc react

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import gql from 'graphql-tag'
+import {graphql} from 'react-apollo'
 
 
 class SongList extends Component{
 render(){
+    console.log(this.props);
     return(<div>
         SongList
     </div>
@@ -20,4 +22,4 @@ const query = gql`
 }
 `;
 
-export default SongList
+export default graphql(query)(SongList)
